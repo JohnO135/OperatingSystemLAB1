fec117d65b4b5f9b0afed3f6f7905de84f421192
JohnO135 <john.ong135@gmail.com>
2019-09-10 10:31:33 -0700

    Second Commit: MBT added

--- OperatingSystemLAB1/OperatingSystemLAB1.cpp
+++ OperatingSystemLAB1/OperatingSystemLAB1.cpp
@@ -7,28 +7,39 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello! This is the Operating System Simulator." << endl;
+	cout << "\nHello! This is the Operating System Simulator." << endl;
 	bool run = 1;
-	
+
+	//This code block will generate the MBT
+	int size = 1024 - 32; //32 represents the bits used for the Operating System
+	cout << "Generating MBT..." << endl;
+	unsigned char MBT[1024];
+	cout << "Flipping OS bits..." << endl;
+	for (int i = 0; i < 32; i++)
+	{
+		MBT[i] == 0x01;
+		cout << MBT[i];
+	}
+
 	while(run)
 	{
 			cout << "Your options are: \n1)Initiate a Process\n2)Print system state\n3)Terminate process with a specific PID\n4)Exit" << endl;
 			int userChoice;
 			cin >> userChoice;
-			if (userChoice < 1 || userChoice > 4)
-			{
-				cout << "\nNot a menu option. Try again.\n" << endl;
-			}
-			if (userChoice == 4)
-			{
-				run = 0;
-			}
-			if (cin.fail())
+			if (cin.fail()) //Checks to ensure the user enters an integer. If not cin is cleared and user is prompted again
 			{
 				cin.clear();
 				cin.ignore();
 				cout << "Not an integer please enter a valid input: " << endl;
 				cin >> userChoice;
+			}
+			if (userChoice == 4) //Will end program. Still needs implementation to terminate current processes.
+			{
+				run = 0;
+			}
+			if (userChoice < 1 || userChoice > 4)
+			{
+				cout << "\nNot a menu option. Try again.\n" << endl;
 			}
 	}
 	cout << "Done" << endl;
